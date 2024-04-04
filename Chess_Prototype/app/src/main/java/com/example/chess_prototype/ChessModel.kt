fa0e3f871743bb5a5faa2a9f3b25351fc11b0eff
iTudor19 <tudor.iftene@yahoo.com>
2024-04-04 05:10:43 +0300

    tabla Catlog

---
+++ Chess_Prototype/app/src/main/java/com/example/chess_prototype/ChessModel.kt
@@ -0,0 +1,18 @@
+package com.example.chess_prototype
+
+class ChessModel {
+    override fun toString(): String {
+        var desc = ""
+        for (row in 7 downTo 0)
+        {
+            desc += "$row"
+            for (col in 0..7)
+                 {
+                     desc += " ."
+                 }
+            desc+="\n"
+        }
+        desc += "  0 1 2 3 4 5 6 7"
+        return desc
+    }
+}
